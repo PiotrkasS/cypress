185377b474374b4c3e5b512c093cc3f3886107cd
PiotrkasS <112478464+PiotrkasS@users.noreply.github.com>
2023-08-15 13:02:10 +0200

    Update Regulaminy.cy.js

--- cypress/e2e/Regulaminy.cy.js
+++ cypress/e2e/Regulaminy.cy.js
@@ -10,16 +10,22 @@ describe('Regulamin', () => {
       cy.wrap(shopsList).each((shop) => {
         cy.log(`Get Terms for: ${shop}`);
         
-        const folderName = `${currentDate}_${shop.replace(/[^\w\s]/gi, '')}`; // Utwórz nazwę folderu zgodną z datą i nazwą sklepu
+        const fileName = `${currentDate}_${shop}`;
+        const txtFileName = `${currentDate}_${shop}.txt`;
+        const folderName = shop.replace(/[^\w\s]/gi, ''); // Usuwa znaki specjalne z nazwy sklepu
         const folderPath = Cypress.config('screenshotsFolder') + '/' + folderName;
-        cy.task('mkdir', folderPath); // Utwórz folder
+        const filePath = '/' + folderName + '/' + fileName;
+        const txtFilePath = folderPath + '/' + txtFileName;
 
-        const fileName = 'screenshot.png'; // Nazwa pliku zrzutu ekranu
-        const txtFileName = 'terms.txt'; // Nazwa pliku tekstowego
-        const filePath = `${folderPath}/${fileName}`;
-        const txtFilePath = `${folderPath}/${txtFileName}`;
+        cy.visit(`https://${shop}/pl/terms.html`, { timeout: 50000 }); // Visit the specific shop's terms page
 
-        cy.visit(`https://${shop}/pl/terms.html`, { timeout: 50000 }); // Odwiedź stronę z regulaminem
+        // Dodana zmiana: Zapisz tekst przed sprawdzeniem przycisku "Akceptuję"
+        cy.get('.text_menu__txt').then((element) => {
+          const text = element.text();
+          cy.log(`Menu Text: ${text}`);
+          cy.writeFile(txtFilePath, text); // Zapisz tekst do pliku
+        });
+
         cy.get('#ckdsclmrshtdwn_v2 > .ck_dsclr__btn_v2, .iai_cookie__box .acceptAll').then((btn) => {
           if (btn.length) {
             cy.wrap(btn).click();
@@ -28,14 +34,14 @@ describe('Regulamin', () => {
         cy.contains('Regulamin').should('be.visible');
         cy.contains('Regulamin').click();
 
-        // Zapisz zrzut ekranu
         cy.screenshot(filePath, { capture: 'fullPage' }).then((screenshotData) => {
           if (typeof screenshotData === 'string' && screenshotData.length > 0) {
-            cy.writeFile(txtFilePath, screenshotData, 'base64').then(() => {
-              // Zapisz tekst do pliku
-              cy.get('.text_menu__txt').then((element) => {
-                const text = element.text();
-                cy.writeFile(txtFilePath, text, { flag: 'a+' }); // Dodaj do pliku tekstowego
+            cy.task('mkdir', folderPath).then(() => {
+              cy.writeFile(filePath, screenshotData, 'base64').then(() => {
+                // Zapisz zrzut ekranu do pliku tekstowego
+                cy.writeFile(txtFilePath, screenshotData).then(() => {
+                  // Poniższy kod jest już usunięty, bo został przeniesiony wyżej
+                });
               });
             });
           } else {
